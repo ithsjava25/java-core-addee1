28f5d39d01187aea8d447c49b238277c0d10ef25
Adam Ottosson <addeecsgo@gmail.com>
2025-10-14 18:16:18 +0200

    Adds category: method that creates a new category or return existing category

---
+++ src/main/java/com/example/Category.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Category {
+    private final String name;
+    private static final Map<String, Category> CACHE = new ConcurrentHashMap<>();
+
+    private Category(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public static Category of(String name){
+        if(name == null){
+            throw new IllegalArgumentException("Category name can't be null");
+        }
+        String trimmedName = name.trim();
+        if (trimmedName.equals("")) {
+            throw new IllegalArgumentException("Category name can't be blank");
+        }
+
+        String normalizedName = trimmedName.substring(0, 1).toUpperCase() + trimmedName.substring(1).toLowerCase();
+
+        // if already exists return, if not, create a new.
+        return CACHE.computeIfAbsent(normalizedName, Category::new);
+
+    }
+}
